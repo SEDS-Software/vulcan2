0ccfcf9e26e3edd74881c054bbb516a294610e25
Alex Forencich <alex@alexforencich.com>
2021-09-16 11:53:30 -0700

    Update GSE ADC EMA

--- firmware/gse/Src/main.c
+++ firmware/gse/Src/main.c
@@ -266,11 +266,15 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
   {
     // exponential moving average
 
-    adc_ref_acc = adc_ref_acc * 0.9999 + (adc1_dma_buffer[0] << 16) * 0.0001;
+    // ref channel init
+    if (adc_ref_acc == 0)
+      adc_ref_acc = adc1_dma_buffer[0] << 16;
+
+    adc_ref_acc = adc_ref_acc * 0.999 + (adc1_dma_buffer[0] << 16) * 0.001;
 
     for (int i = 0; i < 8; i++)
     {
-      adc1_acc[i] = adc1_acc[i] * 0.999 + (adc1_dma_buffer[i+1] << 16) * 0.001;
+      adc1_acc[i] = adc1_acc[i] * 0.95 + (adc1_dma_buffer[i+1] << 16) * 0.05;
     }
   }
 }
